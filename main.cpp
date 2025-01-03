349b1d6f9bf4a1c6145a309c92bbf79718691504
Emanuel Wertman <146956145+ManoW3@users.noreply.github.com>
2025-01-03 14:20:49 +0000

    initial work

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+#include <ctime>
+#include <ctype.h>
+using namespace std;
+
+void introduction() {
+    cout<<"Welcome to the Bank Heist Escape Room. You will be given a set of challenges and clues to be able to successfully infiltrate, loot, and escape a high security bank. Make sure to pay attention to different things like capital letters and use of numbers. These will be your only clues. Good luck!" << endl;
+}
+
+bool distract() {
+    printf("There are guards at the entrenace,luckily you came prepared");
+}
+
+bool enterVault() {
+    
+}
+
+bool disableAlarm() {
+    
+}
+
+int lootVault() {
+    
+}
+
+bool escapeVault() {
+    
+}
+
+bool climbVault() {
+    
+}
+
+bool glide() {
+    
+}
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
