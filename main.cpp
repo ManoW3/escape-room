79372c1fdac3af695fadc86b72d91878941a0364
Emanuel Wertman <146956145+ManoW3@users.noreply.github.com>
2025-01-07 14:20:29 +0000

    worked on functions

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-// #include <cmath>
-// #include <ctime>
-// #include <ctype.h>
+#include <cmath>
+#include <ctime>
+#include <ctype.h>
 #include <string>
+#include <cstdio>
 
 using namespace std;
 
@@ -48,7 +49,7 @@ bool enterVault() {
     cin >> code;
     if (code == 3415) {
         printf("Wow good job! You now enter the vault.\n");
-        printf("there are dIAMonds, Money, platinUm, treaSures, and dICed cheese")
+        printf("there are dIAMonds, Money, platinUm, treaSures, and dICed cheese");
         return true;
     }
     if (!code) {
@@ -56,19 +57,36 @@ bool enterVault() {
         return false;
     }
     else {
-        printf("WRONG ALARM SOUNDING!!!\n");
+        printf("WRONG!!!\n ALARM SOUNDING!!!\n");
         return false;
     }
 }
 
 bool disableAlarm() {
-    int seconds = 20;
-    
+    time_t start = time(nullptr);                     // gets the current time using the things we learned earlier
+    string guess;
+    while (true){
+        
+        if (cin.peek() != EOF) {                          // Checks if the user inputed anything
+            getline(cin, guess);
+            break;
+        }
 
+        time_t current = time(nullptr);
+        printf("%ld\n", current - start);
+        if (current - start >= 3) {
+            printf("You ran out of time and got caught, you will now be going to jail for the rest of your life (he won't drop by then)");
+            return false;
+        }
+    }
+    if (guess == "IAMMUSIC")
+        return true;
+    return false;
 }
 
 // int lootVault() {
-    
+//    srand(time(0));
+//    cout << rand() % 1 << endl;
 // }
 
 // bool escapeVault() {
@@ -76,13 +94,43 @@ bool disableAlarm() {
 // }
 
 // bool climbVault() {
-    
+//     int code2;
+//     cout << "\033[2J\033[1;1H";
+//     printf("You start climbing the wall to escape the bank, but you start to slip. There is a box of food near you, but you must enter the 4 digit code from earlier to regain strength and climb to the roof\nEnter Code: ");
+//     getline(cin, code2);        //User input to see if they remember the code from earlier
+
+//     if(code2 == 3415){          //If the user inputted code is equal to 3415, this will run
+//         printf("Correct! You have regained strength by eating the food from the secret food box. You are now able to climb to the roof of the bank to make your escape!\n");
+//         return true;
+//     }
+//     else{
+//         printf("WRONG!!!\n ALARM SOUNDING!!!\n");
+//         return false;
+//     }
 // }
 
 // bool glide() {
-    
+//     string riddle;
+//     printf("You find Peely on the roof. He says if u can answer his riddle he will give you glider redeploy to glide down to escape the bank successfully.\n'What fortnite emote uses Dababy's song BOP?': ");
+//     cin.ignore();
+//     getline(cin, riddle);                   //allows for spaces in user input
+
+//     if(riddle == "Jabba Switchaway"){        //if user unput is equal to this this will run
+//         printf("Correct! You have successfully escaped the bank!");
+//         return 0;
+//     }
+//     else{                                   //if not this will run
+//         printf("WRONG!!!\n ALARM SOUNDING!!! YOU HAVE BEEN CAUGHT AND SENT TO JAIL\n");
+//         return 0;
+//     }
+
 // }
+
+
 int main() {
-    enterVault();
+
+    bool test = disableAlarm();
+    printf("%d\n", test);
+
     return 0;
 }
